2c788b27f36530f2b227fb6aae4a1dbce7977876
Marantesss <up201706917@fe.up.pt>
2020-10-25 17:40:44 +0000

    feat: add more env variables for axios usage

--- scope-frontend/src/services/api.js
+++ scope-frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const api = () => {
-  let baseUrl = process.env.VUE_APP_ROOT_API;
+  let baseUrl = process.env.VUE_APP_BACKEND_DOMAIN;
+  if (process.env.VUE_APP_BACKEND_PORT != null) {
+    baseUrl += `:${process.env.VUE_APP_BACKEND_PORT}`;
+  }
+
   return axios.create({
     baseURL: baseUrl,
     xsrfCookieName: "csrftoken",

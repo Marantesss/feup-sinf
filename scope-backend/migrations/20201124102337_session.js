bc3a93eae6a47e70f3c01188f4da87eb06ed3492
Marantesss <up201706917@fe.up.pt>
2020-11-26 14:39:26 +0000

    fix: use user's uuid to reference session

--- scope-backend/migrations/20201124102337_session.js
+++ scope-backend/migrations/20201124102337_session.js
@@ -1,13 +1,16 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('session', (table) =>  {
-        // Create Id column of type uuid, primary key, not null, default to postgres generate uuid
-        table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
-        // Using user email to uniquely identify him
-        table.string('email').notNullable().unique();
+exports.up = function (knex) {
+  return knex.schema.createTable('session', (table) => {
+    // Create Id column of type uuid, primary key, not null, default to postgres generate uuid
+    table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    // session table references a user and if that user is deleted so should this row
+    table.uuid('userId').references('id').inTable('user').notNullable().onDelete('CASCADE');
+    // useTz always store data in UTC Format
+    table.timestamp('createdAt', { useTz: true }).defaultTo(knex.fn.now());
+
   })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('session');
 };

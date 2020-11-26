9de9996a6c18e4e33072c430c05f207238ef8faa
Marantesss <up201706917@fe.up.pt>
2020-11-26 15:29:00 +0000

    feat: add migration and seeding scripts + user seeds with encrypted passwords

--- scope-backend/seeds/user.js
+++ scope-backend/seeds/user.js
@@ -5,9 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('user').insert([
-        {id: '9dd6d2af-e5f0-4099-89da-165ce6d93f39', email: 'user1@email.com', password: 'password1'},
-        {id: '95d5112e-6c8b-4fd2-93e7-59f91579f788', email: 'user2@email.com', password: 'password2'},
-        {id: 'e84f3aab-6331-442a-97ec-9021648d9af1', email: 'user3@email.com', password: 'password3'}
+        // passwords are all "password"
+        {id: '9dd6d2af-e5f0-4099-89da-165ce6d93f39', email: 'user1@email.com', password: '$2b$10$BLVNGAiMe5sI6zR4eMBEZuPpiiw72nGHKbVf0924qOKc096uNw/by'},
+        {id: '95d5112e-6c8b-4fd2-93e7-59f91579f788', email: 'user2@email.com', password: '$2b$10$BLVNGAiMe5sI6zR4eMBEZuPpiiw72nGHKbVf0924qOKc096uNw/by'},
+        {id: 'e84f3aab-6331-442a-97ec-9021648d9af1', email: 'user3@email.com', password: '$2b$10$BLVNGAiMe5sI6zR4eMBEZuPpiiw72nGHKbVf0924qOKc096uNw/by'}
       ]);
     });
 };

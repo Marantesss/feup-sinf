70578072f7c49059ff2b4dfef7bee485fa381bae
Raul Viana <raulmanuelviana@gmail.com>
2020-11-29 18:27:17 +0000

    changes in line with latest comments

--- scope-backend/migrations/20201129154551_account.js
+++ scope-backend/migrations/20201129154551_account.js
@@ -2,13 +2,12 @@
 exports.up = function(knex) {
     return knex.schema.createTable('account', (table) => {
         //table with all SAFT account details
-        table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
         table.integer('account_id').notNullable();
         table.string('description').notNullable();
-        table.integer('opening_debit_balance').notNullable();
-        table.integer('opening_credit_balance').notNullable();
-        table.integer('closing_debit_balance').notNullable();
-        table.integer('closing_credit_balance').notNullable();
+        table.float('opening_debit_balance', 6).notNullable();
+        table.float('opening_credit_balance', 6).notNullable();
+        table.float('closing_debit_balance', 6).notNullable();
+        table.float('closing_credit_balance', 6).notNullable();
         table.string('group_category').notNullable();
         table.integer('group_code').notNullable();
         table.integer('taxonomy_code'),notNullable();
@@ -19,12 +18,3 @@ exports.down = function(knex) {
     return knex.schema
     .dropTable('account');
 };
-
-
-
-
-
-
-<GroupingCategory>GM</GroupingCategory>
-<GroupingCode>11</GroupingCode>
-<TaxonomyCode>1</TaxonomyCode>
--- scope-backend/migrations/20201129160121_costumer.js
+++ scope-backend/migrations/20201129160121_costumer.js
@@ -2,13 +2,12 @@
 exports.up = function(knex) {
   return knex.schema.createTable('costumer', (table) => {
         //table with costumer details excetp adresses
-        table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
         table.integer('costumer_id').notNullable();
         table.integer('accountID').notNullable().defaultTo(null).references('account.account_id');
         table.integer('taxID').notNullable();
         table.string('name').notNullable();
-        table.integer('phone').notNullable();
-        table.integer('fax').notNullable()
+        table.string('phone').notNullable();
+        table.string('fax').notNullable()
         table.integer('self_billing_indicator');
         });
 };

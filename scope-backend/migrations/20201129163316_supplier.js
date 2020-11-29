f26a220d08c7a52e905d0c1920a93f76756d241f
Raul Viana <raulmanuelviana@gmail.com>
2020-11-29 18:27:17 +0000

    changes in line with latest comments

--- scope-backend/migrations/20201129163316_supplier.js
+++ scope-backend/migrations/20201129163316_supplier.js
@@ -2,11 +2,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('supplier', (table) => {
         //table with SAFT supplier details except adresses
-        table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
         table.integer('supplier_id').notNullable();
         table.integer('acountID').notNullable().references('account.account_id');
         table.integer('tax_id').notNullable();
-        table.integer('phone').notNullable();
+        table.string('phone').notNullable();
         table.integer('self_billing_indicator').notNullable();
     });
 };

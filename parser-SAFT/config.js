7a3522c4516adb854618a4c4714a90404d2daebc
Marantesss <up201706917@fe.up.pt>
2020-11-30 18:49:42 +0000

    :zap: add node package and knex config

---
+++ parser-SAFT/config.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+config = {};
+
+config.express = {
+  port: process.env.PORT || 8001,
+  timeout: 20000,
+  maxAge: 600
+};
+
+config.db = {
+  client: process.env.DB_CLIENT,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+module.exports = config;

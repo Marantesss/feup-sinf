f469facb60430d9719ba8e96891c86dbf1825f52
Marantesss <up201706917@fe.up.pt>
2020-11-30 19:52:07 +0000

    feat: add more packages and app configurations

--- parser-SAFT/index.js
+++ parser-SAFT/index.js
@@ -1,3 +1,18 @@
-const knex = require('./knex');
+// Initialize app
+const app = {};
 
-console.log(process.env)
+// add knex for db seeding
+app.knex = require('./knex');
+// add parser
+app.parser = require('./parser');
+
+async function test() {
+  const data = await app.parser.readFile(); 
+  console.log(data);
+  
+  return 'return do main'
+  
+}
+
+test().then(v => console.log(v));
+console.log('isto vai fazer log primeiro que os logs do main');

2f219ed45ec6297aa3b3d65230635683232abb71
Marantesss <up201706917@fe.up.pt>
2020-12-03 17:04:24 +0000

    feat: add more stuff to user migration

--- scope-backend/migrations/20201121185153_user.js
+++ scope-backend/migrations/20201121185153_user.js
@@ -1,8 +1,12 @@
 
 exports.up = function (knex) {
   return knex.schema.createTable('user', (table) => {
-    // Create Id column of type uuid, primary key, not null, default to postgres generate uuid
-    table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    // Create Id column of type integer
+    table.integer('id').notNullable().primary();
+    table.string('name').notNullable().unique();
+    table.string('businessName').notNullable();
+    table.string('telephone').notNullable();
+    table.string('fax').notNullable();
     // create email column
     table.string('email').notNullable().unique();
     // create passwword column

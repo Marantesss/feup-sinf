67ca29f8bcee38c54987034e185da5fcf543718f
Paulo Marques <up201705615@fe.up.pt>
2020-12-04 16:31:41 +0000

    setting up the requirements for jasmin communication

--- scope-backend/index.js
+++ scope-backend/index.js
@@ -6,3 +6,21 @@ const { port } = config.express;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+axios.post('https://identity.primaverabss.com/connect/token', {
+  grant_type: 'client_credentials',
+  client_id: 'SCOPEJS',
+  client_secret: process.env.CLIENT_SECRET,
+  scope: application
+})
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+

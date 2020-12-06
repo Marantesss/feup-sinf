a024b6a79c62322ea4f23d64ccd92178b2ec538a
Marantesss <up201706917@fe.up.pt>
2020-12-06 17:47:07 +0000

    linting and some docs

--- scope-backend/util/http.js
+++ scope-backend/util/http.js
@@ -3,31 +3,31 @@ const FormData = require('form-data');
 
 
 const getBodyData = (formObj) => {
-    const bodyData = new FormData();
-    for (const key in formObj) {
-        bodyData.append(key, formObj[key]);
-    }
-    return bodyData;
+  const bodyData = new FormData();
+  for (const key in formObj) {
+    bodyData.append(key, formObj[key]);
+  }
+  return bodyData;
 };
 
 
 const http = (method, url, data) => {
 
-    const bodyData = getBodyData(data);
+  const bodyData = getBodyData(data);
 
-    return axios({
-        baseURL: url,
-        method: method,
-        data: bodyData,
-        headers: { ...bodyData.getHeaders() }
-    });
+  return axios({
+    baseURL: url,
+    method: method,
+    data: bodyData,
+    headers: { ...bodyData.getHeaders() }
+  });
 };
 
 
 const setToken = (token) => {
-    axios.defaults.headers.common = { "Authorization": `bearer ${token}` };
+  axios.defaults.headers.common = { "Authorization": `bearer ${token}` };
 };
 
 module.exports = {
-    http,setToken
+  http, setToken
 };

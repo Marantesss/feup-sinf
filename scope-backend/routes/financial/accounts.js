2c8689b2381f1fa31efddff7cbd582b369896981
Marantesss <up201706917@fe.up.pt>
2020-12-07 14:50:40 +0000

    feat: add query data by month

--- scope-backend/routes/financial/accounts.js
+++ scope-backend/routes/financial/accounts.js
@@ -4,15 +4,15 @@ const router = express.Router();
 
 
 /**
- * Get all accounts
+ * Get all accounts information
  */
 router.get('/', async (req, res) => {
   const accounts = await req.app.knex('account');
-  return res.json({ status: "SUCCESS", accounts });
+  return res.json({ status: 200, accounts });
 });
 
 /**
- * Get specific accounts
+ * Get specific account's total debit and credit
  * 
  * Query Strings
  *  - startDate
@@ -42,8 +42,42 @@ router.get('/:id', async (req, res) => {
     .sum({ totalDebit: 'amount' })
     .first();
 
-  return res.json({ status: "SUCCESS", totalCredit, totalDebit });
+  return res.json({ status: 200, totalCredit, totalDebit });
+});
 
+/**
+ * Get specific account's total debit and credit by month
+ * 
+ * Query Strings
+ *  - startDate
+ *  - endDate
+ */
+router.get('/:id/monthly', async (req, res) => {
+  const accountId = req.params.id;
+
+  const data = [];
+  for (const month of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
+    // get all credit lines
+    const { totalCredit } = await req.app.knex('line')
+      .join('transaction', 'line.transactionId', 'transaction.id')
+      .whereRaw(`EXTRACT(MONTH FROM transaction.date) = ${month}`)
+      .andWhere('line.accountId', accountId)
+      .andWhere('line.type', 'credit')
+      .sum({ totalCredit: 'amount' })
+      .first();
+    // get all debit lines
+    const { totalDebit } = await req.app.knex('line')
+      .join('transaction', 'line.transactionId', 'transaction.id')
+      .whereRaw(`EXTRACT(MONTH FROM transaction.date) = ${month}`)
+      .andWhere('line.accountId', accountId)
+      .andWhere('line.type', 'debit')
+      .sum({ totalDebit: 'amount' })
+      .first();
+
+    data.push({totalCredit, totalDebit});
+  }
+
+  return res.json({ status: 200, data });
 });
 
 module.exports = router;

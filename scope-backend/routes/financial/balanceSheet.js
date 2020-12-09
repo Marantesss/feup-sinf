314ee1e79492624004c21fdeb8ab0eafb0ab4378
Marantesss <up201706917@fe.up.pt>
2020-12-09 16:50:55 +0000

    feat: add function for calculating balance of all acounst with a certain taxonomy

--- scope-backend/routes/financial/balanceSheet.js
+++ scope-backend/routes/financial/balanceSheet.js
@@ -1,52 +1,28 @@
 const express = require('express');
+const app = require('../../app');
 
 const router = express.Router();
+
+const { balanceSheet } = require('../../util/financial');
 
 /**
  * Get balance sheet data
  */
 router.get('/', async (req, res) => {
 
-  /**
-  * 
-  * @param {Array} accounts 
-  */
-  const getAccountsBalance = async (accounts) => {
+  const getTaxonomyBalance = async (taxonomy) => {
     const { balance } = await req.app.knex('account')
       .select(req.app.knex.raw('SUM("closingDebitBalance" - "closingCreditBalance") as "balance"'))
-      .whereIn('id', accounts)
+      .where('taxonomyCode', taxonomy)
       .first();
 
+    console.log(balance);
+
     return balance;
-  }
-
-  const balanceSheet = {
-    assets: { // Ativo
-      current: { // Ativo corrente
-        cash: await getAccountsBalance([11, 12, 13, 14]),
-        clients: await getAccountsBalance([21]),
-
-      },
-      nonCurrent: { // Ativo nao corrente
-
-      },
-      total: 0,
-    },
-    liabilities: { // Passivo
-      current: { // Passivo corrente
-        suppliers: await getAccountsBalance([22]),
-        tax: await getAccountsBalance([24])
-      },
-      nonCurrent: { // Passivo nao corrente
-        debt: await getAccountsBalance([23]),
-        funding: await getAccountsBalance([25]),
-      },
-      total: 0,
-    },
-    equity: { // Capital Proprio
-      total: 0,
-    }
   };
+
+  return res.json({ status: 200, balance: await getTaxonomyBalance(2) });
+
 });
 
 module.exports = router;

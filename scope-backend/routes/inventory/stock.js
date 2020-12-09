068f95e2f07e21558512924a838e386fc2dfe9f8
Paulo Marques <up201705615@fe.up.pt>
2020-12-09 23:08:49 +0000

    added some detail to the stock request

--- scope-backend/routes/inventory/stock.js
+++ scope-backend/routes/inventory/stock.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const jasmin = require('../../util/jasmin');
 const router = express.Router();
-const { getStockValue} = require('../../util/stock');
+const { getStockValue, getItemInfo} = require('../../util/stock');
 
 
 router.get('/', async (_req, res) => {
     jasmin.jasminRequest('get', 'materialscore/materialsitems').then(
         (stockData) => {
-            const response = { stockValue: 0 }
+            const response = { stockValue: 0, materialItems: [] }
             stockData.forEach((materialItem) => {
                 response.stockValue += getStockValue(materialItem);
+                response.materialItems.push(getItemInfo(materialItem));
             })
             res.json(response);
         }
--- scope-backend/util/stock.js
+++ scope-backend/util/stock.js
@@ -2,23 +2,7 @@
 
 //Assumes it gets an item in the  array return in   /materialscore/materialsitems
 
-/*
-const getStockQuantity = (item) => {
 
-    return item.materialsItemWarehouses.reduce((accumulator, currValue) => {
-        accumulator += currValue.stockBalance;
-        return accumulator;
-    }, 0);
-
-}
-    // Gets an average if for some reason the warehouses have a different unit price 
-const getUnitPrice = (item) =>{
-    return (item.materialsItemWarehouses.reduce((accumulator, currValue) => {
-        accumulator += currValue.calculatedUnitCost.amount;
-        return accumulator;
-    }, 0) / item.materialsItemWarehouses.length);
-}
-*/
 const getStockValue = (item) => {
 
     return item.materialsItemWarehouses.reduce((accumulator,currValue) =>{
@@ -28,12 +12,39 @@ const getStockValue = (item) => {
 
 }
 
+const getItemInfo = (item) => {
+    
+    const info =  {};
+
+    info.itemKey = item.itemKey;
+    info.description = item.description;
+    info.baseUnitDescription = item.baseUnitDescription;
+    info.warehouses = [];
+
+    
+    item.materialsItemWarehouses.forEach(element => {
+        const warehouse = {};
+        
+        warehouse.warehouseId = element.warehouseId;
+        warehouse.warehouseDescription = element.warehouseDescription;
+        warehouse.stock = element.stockBalance;
+        warehouse.basePrice = element.calculatedUnitCost.amount;
+        info.warehouses.push(warehouse);
+    });
+    
+
+
+
+    return info
+}
+
 
 
 ///
 
 module.exports = {
-    getStockValue
+    getStockValue,
+    getItemInfo
 
 
 }

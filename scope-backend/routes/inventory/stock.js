3a5be3ff45ce98b0f18307407b61c53e0924dc0d
Paulo Marques <up201705615@fe.up.pt>
2020-12-09 23:39:05 +0000

    splited the previous request into two different routes

--- scope-backend/routes/inventory/stock.js
+++ scope-backend/routes/inventory/stock.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const jasmin = require('../../util/jasmin');
 const router = express.Router();
-const { getStockValue, getItemInfo} = require('../../util/stock');
+const { getStockValue, getItemInfo } = require('../../util/stock');
 
 
 router.get('/', async (_req, res) => {
     jasmin.jasminRequest('get', 'materialscore/materialsitems').then(
         (stockData) => {
-            const response = { stockValue: 0, materialItems: [] }
+            const response = {materialItems: [] }
+            stockData.forEach((materialItem) => {
+                response.materialItems.push(getItemInfo(materialItem));
+            })
+            res.json(response);
+        }
+
+    ).catch(
+        () => {
+            let err = new Error("Failed to retrive stock itens");
+            err.status = 500;
+            res.json({
+                message: err.message,
+                error: err
+            });
+
+        }
+    )
+
+
+
+
+})
+
+router.get('/value', async (_req, res) => {
+
+    jasmin.jasminRequest('get', 'materialscore/materialsitems').then(
+        (stockData) => {
+            const response = { stockValue: 0}
             stockData.forEach((materialItem) => {
                 response.stockValue += getStockValue(materialItem);
-                response.materialItems.push(getItemInfo(materialItem));
             })
             res.json(response);
         }
@@ -23,7 +50,7 @@ router.get('/', async (_req, res) => {
                 message: err.message,
                 error: err
             });
-  
+
         }
     )
 
@@ -31,7 +58,6 @@ router.get('/', async (_req, res) => {
 
 
 })
-
 
 
 module.exports = router;

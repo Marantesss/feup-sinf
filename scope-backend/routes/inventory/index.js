ac7f0b57986586984afd846ae4e3c2be505500db
Paulo Marques <up201705615@fe.up.pt>
2020-12-10 01:11:24 +0000

    🚧 Added what will be the product route for the drilldown 🚧

--- scope-backend/routes/inventory/index.js
+++ scope-backend/routes/inventory/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { authenticate } = require('../../middlewares')
 
 const stockRouter = require('./stock');
+const productRouter = require('./product');
 
 // routes are protected with user authentication
 //router.use(authenticate);
@@ -11,5 +12,6 @@ router.get('/', (req, res) => {
   return res.json({ status: 200, message: 'Inventory Index' });
 });
 router.use('/stock', stockRouter);
+router.use('/product',productRouter);
 
 module.exports = router;

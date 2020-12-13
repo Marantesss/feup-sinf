70c530c4c592d4b0f90b21a61466e50f0d27a640
Marantesss <up201706917@fe.up.pt>
2020-12-13 17:07:18 +0000

    add mixin and ebit in financial

--- scope-frontend/src/components/finances/BalanceSheet.vue
+++ scope-frontend/src/components/finances/BalanceSheet.vue
@@ -58,9 +58,12 @@
 
 <script>
 import api from "@/services/api";
+import currencyFormatter from "@/mixins/currencyFormatter";
 
 export default {
   name: "BalanceSheet",
+
+  mixins: [currencyFormatter],
 
   methods: {
     filterOnlyCapsText(value, search) {
@@ -73,39 +76,6 @@ export default {
           .toLocaleUpperCase()
           .indexOf(search.toString().toLocaleUpperCase()) !== -1
       );
-    },
-
-    formatCurrency(amount) {
-      let decimalCount = 2;
-      const decimal = ".";
-      const thousands = " ";
-      const moneySign = "€ ";
-      try {
-        decimalCount = Math.abs(decimalCount);
-        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
-
-        const negativeSign = amount < 0 ? "-" : "";
-
-        let i = parseInt(
-          (amount = Math.abs(Number(amount) || 0).toFixed(decimalCount))
-        ).toString();
-        let j = i.length > 3 ? i.length % 3 : 0;
-
-        return (
-          moneySign +
-          negativeSign +
-          (j ? i.substr(0, j) + thousands : "") +
-          i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) +
-          (decimalCount
-            ? decimal +
-              Math.abs(amount - i)
-                .toFixed(decimalCount)
-                .slice(2)
-            : "")
-        );
-      } catch (e) {
-        console.log(e);
-      }
     },
   },
 
